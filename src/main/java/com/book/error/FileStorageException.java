820e7f966c2017946624c5ae6b606455d1895cc8
Ahmed  Elsheikh <ahmedelsheikh680@gmail.com>
2022-10-02 15:42:29 -0700

    upload file #1

---
+++ src/main/java/com/book/error/FileStorageException.java
@@ -0,0 +1,14 @@
+package com.book.error;
+
+public class FileStorageException extends RuntimeException{
+    public FileStorageException() {
+    }
+
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
