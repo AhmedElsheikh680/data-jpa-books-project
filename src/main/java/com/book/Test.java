e231cd80669f96ba8e7220b1a998c1b6e99ae404
Ahmed Elsheikh <ahmed.elsheikh@AlAhlyMomKn.com>
2022-11-01 16:43:08 +0200

    updated

--- src/main/java/com/book/Test.java
+++ src/main/java/com/book/Test.java
@@ -1,23 +1,43 @@
 package com.book;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Test {
-//    static void print() {
-//        while (true){
-//            System.out.println("Hello World");
-//            try {
-//                Thread.sleep(2000);
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//    }
+
 
     public static void main(String[] args) {
+        List<Person> myList = new ArrayList<>();
+        myList.add(new Person(10, "Ahmed"));
+        myList.add(new Person(5, "Mohamed"));
+        myList.add(new Person(20, "Dawi"));
+        System.out.println(
+                myList.stream().map(p->p.getNa)
+        );
 
-//        print();
-        int max = Integer.MAX_VALUE;
-        int min = Integer.MIN_VALUE;
-        System.out.printf("Max: "+ max);
-        System.out.printf("Min: "+ min);
+    }
+
+
+
+}
+
+class Person implements Comparable<Person> {
+    int age;
+    String name;
+
+    public Person(int age, String name) {
+        this.age = age;
+        this.name = name;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
     }
 }
+
